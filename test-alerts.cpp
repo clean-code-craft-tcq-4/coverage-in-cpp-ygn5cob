9dd46be95a68ff7d0c84b20b700d36cf737f3f8c
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-07-20 18:25:48 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -21,7 +21,7 @@ TEST_CASE("classift temperate breach") {
 TEST_CASE("check and alert") {
   BatteryCharacter batteryChar;
   batteryChar.coolingType = PASSIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER, batteryChar, 20);
-  checkAndAlert(TO_EMAIL, batteryChar, 20);
-  checkAndAlert(TO_EMAIL, batteryChar, 40);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 20) == true);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 20) == true);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 40) == true);
 }
